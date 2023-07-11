36f287605c9128b42113dda0c5757c5bca5c23d7
Manoj Patil <manoj.patil@baptisthealth.net>
2023-07-11 11:54:02 +0530

    added to create accounts

---
+++ terraform/security.tf
@@ -0,0 +1,27 @@
+module "network" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "securityservice.in@capgemini.com"
+    AccountName               = "security-service"
+    ManagedOrganizationalUnit = "Security"
+    SSOUserEmail              = "securityservice.in@capgemini.com"
+    SSOUserFirstName          = "Security"
+    SSOUserLastName           = "Service"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "security"
+}
