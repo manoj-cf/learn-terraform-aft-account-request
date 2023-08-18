90dbf58bd04aba875cca958373ae6c3be1ba9313
Manoj Patil <manoj.patil@baptisthealth.net>
2023-08-18 14:18:36 +0530

    Clean for Log acc

--- terraform/log.tf
+++ terraform/log.tf
@@ -1,27 +1,27 @@
-module "log" {
-  source = "./modules/aft-account-request"
+# module "log" {
+#   source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "cflogservice2.in@capgemini.com"
-    AccountName               = "log-service-acc"
-    ManagedOrganizationalUnit = "Infra"
-    SSOUserEmail              = "cflogservice2.in@capgemini.com"
-    SSOUserFirstName          = "Log"
-    SSOUserLastName           = "Service"
-  }
+#   control_tower_parameters = {
+#     AccountEmail              = "cflogservice2.in@capgemini.com"
+#     AccountName               = "log-service-acc"
+#     ManagedOrganizationalUnit = "Infra"
+#     SSOUserEmail              = "cflogservice2.in@capgemini.com"
+#     SSOUserFirstName          = "Log"
+#     SSOUserLastName           = "Service"
+#   }
 
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
+#   account_tags = {
+#     "Learn Tutorial" = "AFT"
+#   }
 
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
+#   change_management_parameters = {
+#     change_requested_by = "HashiCorp Learn"
+#     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+#   }
 
-  custom_fields = {
-    group = "non-prod"
-  }
+#   custom_fields = {
+#     group = "non-prod"
+#   }
 
-  account_customizations_name = "log"
-}
+#   account_customizations_name = "log"
+# }
